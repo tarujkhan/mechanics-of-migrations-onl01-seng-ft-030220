e9138009a2e98af4a77e9f2b3362b1c5d67c2b75
taroj khan <tarujkhan@yahoo.com>
2020-04-08 16:16:50 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,5 +1,5 @@
 class AddFavoriteFoodToArtists < ActiveRecord::Migration[4.2]
   def change 
-    add_column :artists, :avorite_food, :string 
+    add_column :artists, :favorite_food, :string 
   end 
 end 
